a13436b86180f844e2852e7d2e96f3d6b23e242a
glenk@galkam.com.au <glenk@galkam.com.au>
2017-07-11 09:15:35 +1000

    Completed Context Based Labels. Refactored Mini Test Framework Deprecated TLabelledEnum.SetLabels.  Renamed Mini Test Framework t"SetTestCase" to "NewTestCase" for consistency.

--- Enum.pas
+++ Enum.pas
@@ -4,6 +4,8 @@ interface
 
 uses System.SysUtils, System.TypInfo, System.Rtti;
 
+const ENUM_CONTEXT_DEFAULT = 'EnumContextDefault';
+
 Type
 
   TLabelAliases = TArray<string>;
@@ -11,13 +13,14 @@ Type
 
   TLabelledEnum<T> = Record
   private
+    fContext: string;
     fLabels : PLabelAliases;
     fTypeName: string;
     Procedure SetType;
     Function GetEnumTypeName: string;
     function GetTypeName: string;
-    procedure setLabels(const Value: TLabelAliases);
-    function getLabels: TLabelAliases;
+    procedure setLabels(const Value: TLabelAliases); deprecated;
+    function  getLabels: TLabelAliases;
   public
     InternalTypeInfo: PTypeInfo;
     Enum: T;
@@ -26,7 +29,11 @@ Type
     Property TypeName: string read GetEnumTypeName;
     Function ValueName: string;
     Property Labels: TLabelAliases read getLabels write setLabels;
+    Property Context : string read fContext write fContext;
     Class function AsString(AType: T): string; static;
+    Class Procedure SetTypeLabels(ALabels: TArray<String>; AContext : string = ENUM_CONTEXT_DEFAULT); static;
+    Class Function GetTypeLabels(AContext : string = ENUM_CONTEXT_DEFAULT): TArray<String>; static;
+    Class Procedure ClearTypeLabels(AContext: string = ENUM_CONTEXT_DEFAULT); static;
     Class Operator Implicit(AType: TLabelledEnum<T>): string;
     Class operator Implicit(ALabel: String): TLabelledEnum<T>;
     Class operator Implicit(AType: T): TLabelledEnum<T>;
@@ -44,45 +51,111 @@ Type
 // GetAliasEnumValue called "GetAliasEnumName(TypeInfo: PTypeInfo; const Integer): string
 // But, because it doesn't we have to keep our own Factory implementation here.
   TLabelledEnumAliasEntry = Record
+    Context: String;
     InternalTypeInfo: PTypeInfo;
     AliasArray: TLabelAliases;
   End;
 var LabelledEnumAliases :TArray<TLabelledEnumAliasEntry>;
 
-Procedure RegisterEnumAlias(Const ATypeInfo:PTypeInfo; AAliases: TLabelAliases);
+Procedure RegisterEnumAlias(Const ATypeInfo:PTypeInfo; AAliases: TLabelAliases;
+           AContext: String=ENUM_CONTEXT_DEFAULT);
 Function GetEnumNameList(ATypeInfo: PTypeInfo; AStart: Integer=0;
         ACount: Integer = -1): TArray<String>;
 Function GetEnumAliasList(ATypeInfo:PTypeInfo; Astart: Integer=0;
-        ACount: Integer = -1): TArray<String>;
-
+        ACount: Integer = -1; AContext: String = ENUM_CONTEXT_DEFAULT): TArray<String>;
+procedure DeRegisterEnumAlias(ATypeInfo: PTypeINfo; AAll: boolean = false;
+         AContext:string = ENUM_CONTEXT_DEFAULT);
 
 implementation
   uses AnsiStrings;
 
-Procedure RegisterEnumAlias(Const ATypeInfo:PTypeInfo; AAliases: TLabelAliases);
+Procedure RegisterEnumAlias(Const ATypeInfo:PTypeInfo; AAliases: TLabelAliases;
+    AContext: string=ENUM_CONTEXT_DEFAULT);
 var
    lLabelEntry:TLabelledEnumAliasEntry;
    lTargetName: string;
-   l, i : integer;
+   lFirstEmpty, l, i : integer;
 begin
+   //note: If you want to change labels for context,
+   // you need to remove it first.
+   // Trying to add context that is already set will be ignored.
+
    if (ATypeInfo=nil) or (AAliases=nil) then exit;
 
    lTargetName := string(AtypeInfo.Name);
-
-   // Check For Duplicate
-   for lLabelEntry in LabelledEnumAliases do
-     if SameText(lTargetName, string(lLabelEntry.InternalTypeInfo.Name)) then exit;
+   lFirstEmpty := -1;
+   // Check For Duplicate and Empty Slots to re-use
    l := length(LabelledEnumAliases);
-   setlength(LabelledEnumAliases,l+1);
+   for I := 0 to l-1 do
+   begin
+     lLabelEntry := LabelledEnumAliases[i];
+     if lLabelEntry.InternalTypeInfo=nil then
+     begin
+       if lFirstEmpty=-1 then lFirstEmpty := i;
+       continue;
+     end;
+     if SameText(lTargetName, string(lLabelEntry.InternalTypeInfo.Name)) and
+        SameText(AContext, lLabelEntry.Context) then exit;
+   end;
+
+   if lFirstEmpty<>-1 then
+     l := lFirstEmpty
+   else
+     setlength(LabelledEnumAliases,l+1);
+   // we Probably dont need to duplicate here as it dynamic
+   // arrays are reference counted.  REMOVE LATER???
    LabelledEnumAliases[l].InternalTypeInfo := ATypeInfo;
+   LabelledEnumALiases[l].Context := AContext;
    setlength(LabelledEnumAliases[l].AliasArray,length(AAliases));
    for i := Low(AAliases) to High(AAliases) do
      LabelledEnumAliases[l].AliasArray[i] := AAliases[i];
+
+   System.TypInfo.AddEnumElementAliases(ATypeInfo, LabelledEnumAliases[l].AliasArray);
+
 end;
 
+procedure DeRegisterEnumAlias(ATypeInfo: PTypeINfo; AAll: boolean = false;
+         AContext:string = ENUM_CONTEXT_DEFAULT);
+var
+   lTargetName: string;
+   i,l: integer;
+begin
+  //not thread safe as yet.
+   if (ATypeInfo=nil) then exit;
+
+   lTargetName := string(AtypeInfo.Name);
+   l := length(LabelledEnumAliases);
+
+   for I := 0 to l-1 do
+   begin
+     if (SameText(lTargetName, string(LabelledEnumAliases[i].InternalTypeInfo.Name))) and
+        ( (AAll) or (SameText(AContext, LabelledEnumAliases[i].Context))) then
+     begin
+       setlength(LabelledEnumAliases[i].AliasArray,0);
+       LabelledEnumAliases[i].Context := '';
+       LabelledEnumAliases[i].InternalTypeInfo := nil;
+       break;
+     end;
+   end;
+
+   // Remove references in the Type
+   RemoveEnumElementAliases(ATypeInfo);
+   if AAll then exit;
+
+   // Add Back any we still need
+   l := length(LabelledEnumAliases);
+   for i := 0 to l-1 do
+   begin
+     if LabelledEnumAliases[i].Context='' then continue;
+     if (LabelledEnumAliases[i].InternalTypeInfo<>Nil) and
+       (SameText(lTargetName, string(LabelledEnumAliases[i].InternalTypeInfo.Name))) then
+       System.TypInfo.AddEnumElementAliases(
+          ATypeInfo, LabelledEnumAliases[i].AliasArray );
+   end;
+end;
 
 Function GetEnumAliasList(ATypeInfo:PTypeInfo; Astart: Integer=0;
-        ACount: Integer = -1): TLabelAliases;
+        ACount: Integer = -1; AContext: string = ENUM_CONTEXT_DEFAULT): TLabelAliases;
 var
    lLabelEntry:TLabelledEnumAliasEntry;
    i,p,lEnd: Integer;
@@ -97,7 +170,9 @@ begin
      // just use the Pointer directly eg
      // if AtypeInfo=lLabelEntry.TypeInfo, but just in case, we'll check
      // the Type Name
-     if (AtypeInfo=lLabelEntry.InternalTypeInfo) and (AtypeInfo.Name = lLabelEntry.InternalTypeInfo.Name) then
+     if (AtypeInfo=lLabelEntry.InternalTypeInfo) and
+        (AContext=lLabelEntry.Context) and
+        (AtypeInfo.Name = lLabelEntry.InternalTypeInfo.Name) then
      begin
        setlength(Result,(lEnd-AStart+1));
        p := -1;
@@ -157,7 +232,7 @@ begin
   AType.checkType;
   v := AType;
 
-  lAliases := GetEnumAliasList(AType.InternalTypeInfo,v,1);
+  lAliases := GetEnumAliasList(AType.InternalTypeInfo,v,1,AType.Context);
   if length(lAliases)>=1 then
   begin
     Result := lAliases[0];
@@ -210,8 +285,14 @@ end;
 
 procedure TLabelledEnum<T>.Clear;
 begin
+  self.fContext := ENUM_CONTEXT_DEFAULT;
   self.fTypeName := '';
   self.GetTypeName;
+end;
+
+class procedure TLabelledEnum<T>.ClearTypeLabels(AContext: string);
+begin
+  DeRegisterEnumAlias(TypeInfo(T),false,AContext);
 end;
 
 class operator TLabelledEnum<T>.Equal(AType: TLabelledEnum<T>;
@@ -236,7 +317,13 @@ end;
 function TLabelledEnum<T>.getLabels: TLabelAliases;
 begin
   CheckType;
-  Result:= GetEnumAliasList(InternalTypeInfo,0);
+  Result:= GetEnumAliasList(InternalTypeInfo,0,-1,FContext);
+end;
+
+class function TLabelledEnum<T>.GetTypeLabels(AContext: string): TArray<String>;
+begin
+  Setlength(Result,0);
+  result := GetEnumAliasList(TypeInfo(T),0,-1,AContext);
 end;
 
 function TLabelledEnum<T>.GetTypeName: string;
@@ -278,11 +365,11 @@ end;
 procedure TLabelledEnum<T>.setLabels(const Value: TLabelAliases);
 begin
   CheckType;
-  if Not (self.fLabels = nil) then RemoveEnumElementAliases(InternalTypeInfo);
-  System.TypInfo.AddEnumElementAliases(InternalTypeInfo, Value);
+//  if Not (self.fLabels = nil) then RemoveEnumElementAliases(InternalTypeInfo);
+//  System.TypInfo.AddEnumElementAliases(InternalTypeInfo, Value);
   // again because we cant directly access the system.typeinfo's copy of the
   // EnumALiases array, we will need to keep a copy
-  RegisterEnumAlias(InternalTypeInfo,Value);
+  RegisterEnumAlias(InternalTypeInfo,Value,fContext);
 end;
 
 procedure TLabelledEnum<T>.SetType;
@@ -290,6 +377,14 @@ begin
   Self.fLabels := nil;
   self.InternalTypeInfo := Typeinfo(T);
   fTypeName := System.TypInfo.GetTypeName(InternalTypeInfo);
+  if Self.fContext='' then self.fContext := ENUM_CONTEXT_DEFAULT;
+  
+end;
+
+class procedure TLabelledEnum<T>.SetTypeLabels(ALabels: TArray<String>;
+  AContext: string);
+begin
+  RegisterEnumAlias(TypeInfo(T), ALabels, AContext);
 end;
 
 function TLabelledEnum<T>.ValueName: string;
